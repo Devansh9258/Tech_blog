696f11113a9feefd5702f77263e53ab85cab0950
Devanshmehra <devanshmehra826@gmail.com>
2024-09-05 19:03:04 +0530

    Added More Post

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+function formatDate(date) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
+
+document.querySelectorAll(".postDate").forEach((dateElement) => {
+  dateElement.innerText = formatDate(new Date());
+});
+*/
